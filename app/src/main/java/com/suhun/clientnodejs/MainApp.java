917bd0907a013c212cae512bba2957455ab81884
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-10-27 09:38:52 +0800

    Add RequestQueue in MainApp when lifecycle onCreate

--- app/src/main/java/com/suhun/clientnodejs/MainApp.java
+++ app/src/main/java/com/suhun/clientnodejs/MainApp.java
@@ -2,6 +2,15 @@ package com.suhun.clientnodejs;
 
 import android.app.Application;
 
-public class MainApp extends Application {
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
+public class MainApp extends Application {
+    public RequestQueue queue;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        queue = Volley.newRequestQueue(this);
+    }
 }
