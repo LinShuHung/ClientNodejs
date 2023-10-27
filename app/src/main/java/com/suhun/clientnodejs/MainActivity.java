a605f9c199e19021707455aed471c329a3dd8942
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-10-27 11:26:30 +0800

    Add StringRequest to Volley, when user press button to send request to server

--- app/src/main/java/com/suhun/clientnodejs/MainActivity.java
+++ app/src/main/java/com/suhun/clientnodejs/MainActivity.java
@@ -1,16 +1,52 @@
 package com.suhun.clientnodejs;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    private String tag = MainActivity.class.getSimpleName();
     private MainApp mainApp;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mainApp = (MainApp) getApplication();
+    }
+
+    public void sendRequestFun(View view){
+        String url = "http://localhost:5000/";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d(tag, "----response"+response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                return super.getParams();
+            }
+        };
+        mainApp.queue.add(stringRequest);
     }
 }
